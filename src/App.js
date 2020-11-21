1db2d6daf853ba4f869a1634130aa771dc123adb
Jong Hyeon Yeo <37054916+mindgitrwx@users.noreply.github.com>
2020-11-21 23:56:57 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h4>I'm ...</h4>
+      <h1>mindgitrwx</h1>
+      <h3></h3>      <h1>Awarded</h1>     {" "}
+      <p>
+        Minister of Defense Award <br></br>
+        Open source Academy for Military<br></br>
+        <br></br>
+        <a href="https://osam.kr/board/read.jsp?id=4266&code=notice">
+          Osam Link
+        </a>
+        <br></br>
+        <a href="https://github.com/osamhack2020/Infra_Meditact_Meditact">
+          Github Link
+        </a>
+      </p>
+    </div>
+  );
+}
